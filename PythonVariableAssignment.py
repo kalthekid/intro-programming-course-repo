6e4da5e159a60ef66320606027b05d4f24fc2d2e
kalthekid <kidkalkidan1234@gmail.com>
2026-09-22 12:54:08 -0400

    Add files via upload

---
+++ PythonVariableAssignment.py
@@ -0,0 +1,46 @@
+# I keep forgetting all my calculations! Let's try keeping them using Python
+
+# 5 times 6
+print("Multiplication")
+number1 = int(input("Please enter the first number: \n"))
+number2 = int(input("Please enter the second number: \n"))
+answer1 = int(number1 * number2)
+print("The answer to your query is: \n",answer1)
+
+# 8 divided by 3
+print("\n Division")
+number1 = int(input("Please enter the first number: \n"))
+number2 = int(input("Please enter the second number: \n"))
+answer2 = float(number1 / number2)
+print("The answer to your query is: \n",answer2)
+
+# 8 divided by 3 w/o remainder
+print("\n Division without remainder")
+number1 = int(input("Please enter the first number: \n"))
+number2 = int(input("Please enter the second number: \n"))
+answer3 = int(number1 // number2)
+print("The answer to your query is: \n",answer3)
+
+# 8 divided by 3 only remainder
+print("\n Division with only remainder")
+number1 = int(input("Please enter the first number: \n"))
+number2 = int(input("Please enter the first number: \n"))
+answer4 = int(number1 % number2)
+print("The answer to your query is: \n",answer4)
+
+# 4200000 plus 23090
+print("\n Addition")
+number1 = int(input("Please enter the first number: \n"))
+number2 = int(input("Please enter the second number: \n"))
+answer5 = int(number1 + number2)
+print("The answer to your query is: \n",answer5)
+
+# 4200000 minus 7633
+print("\n Subtraction")
+number1 = int(input("Please enter the first number: \n"))
+number2 = int(input("Please enter the second number: \n"))
+answer6 = int(number1 - number2)
+print("The answer to your query is: \n",answer6)
+
+# My name
+print("\n Kal Mesfin")
